35d41ba77649ab34b5adc226dd17bb77ecf2b174
Darren Ackles <ackles.darren18@gmail.com>
2024-07-14 18:57:30 -0400

    Had to reset because of a file error

---
+++ C968_Task1/AddParts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace C968_Task1
+{
+    public partial class addPartsForm : Form
+    {
+        public addPartsForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ C968_Task1/AddPartss.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace C968_Task1
+{
+    public partial class addPartsForm : mainForm
+    {
+
+        public addPartsForm()
+        {
+            InitializeComponent();
+
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (addPartsInHouseButton.Checked)
+            {
+                addPartMachineIDLabel.Text = "Machine ID";
+            }
+            else
+            {
+                addPartMachineIDLabel.Text = "Company Name";
+            }
+        }
+
+        private void addPartsSaveButton_Click(object sender, EventArgs e)
+        {
+
+
+
+        }
+    }
+}
