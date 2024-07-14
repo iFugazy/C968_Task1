5f00704d525eecb6b8f768d9f92ba908838516c6
Darren Ackles <ackles.darren18@gmail.com>
2024-07-13 20:09:57 -0400

    Added button labels to the buttons

--- C968_Task1/Form1.Designer.cs
+++ C968_Task1/Form1.Designer.cs
@@ -181,6 +181,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.ClientSize = new System.Drawing.Size(1225, 418);
             this.Controls.Add(this.productsDeleteButton);
             this.Controls.Add(this.productsModifyButton);
